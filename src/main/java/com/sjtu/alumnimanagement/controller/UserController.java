0f327bd7ae4e7c09f96c39cd3df2d61597d033bf
sinkinben <sinkinben@outlook.com>
2021-08-01 21:14:44 +0800

    add comments

--- src/main/java/com/sjtu/alumnimanagement/controller/UserController.java
+++ src/main/java/com/sjtu/alumnimanagement/controller/UserController.java
@@ -11,11 +11,14 @@ import java.util.List;
 @Controller
 @RestController
 @RequestMapping(path = "/user")
+// 进入这个 controller 需要是 localhost:8081/user/xxxx 这样的 URL
+// 如果 localhost 不行, 尝试 127.0.0.1:8081/user/xxxx
 public class UserController {
     @Autowired
     UserInfoService userInfoService;
 
 
+    // localhost:8081/user/getAll
     @CrossOrigin
     @GetMapping(path = "/getAll")
     public List<UserInfo> getAllUserInfo(){
