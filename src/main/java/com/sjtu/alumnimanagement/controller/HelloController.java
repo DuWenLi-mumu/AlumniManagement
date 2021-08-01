cd2da35deef3b7f98fca7cda8bf4b9d3ad237e34
duwenli <duwenli@bytedance.com>
2021-08-01 15:58:42 +0800

    Initial commit

---
+++ src/main/java/com/sjtu/alumnimanagement/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sjtu.alumnimanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String get(){
+        return "hello Spring Boot!";
+    }
+
+}
